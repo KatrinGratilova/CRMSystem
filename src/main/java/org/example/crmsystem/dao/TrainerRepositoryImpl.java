404ad8010e731dd420e451dc299d7067765484ce
Kateryna Hratilova <gr.katrin.05@gmail.com>
2025-01-27 23:23:54 +0200

    finished repositories

--- src/main/java/org/example/crmsystem/dao/TrainerRepositoryImpl.java
+++ src/main/java/org/example/crmsystem/dao/TrainerRepositoryImpl.java
@@ -1,22 +1,41 @@
 package org.example.crmsystem.dao;
 
-import org.example.crmsystem.model.Trainee;
+import org.example.crmsystem.Storage;
+import org.example.crmsystem.dao.interfaces.TrainerRepository;
+import org.example.crmsystem.model.Trainer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Optional;
 
 @Repository
 public class TrainerRepositoryImpl implements TrainerRepository {
-    @Override
-    public Trainee add(Trainee trainee) {
-        return null;
+    private final HashMap<Long, Trainer> trainerStorage;
+    private long idCounter;
+
+    @Autowired
+    public TrainerRepositoryImpl(Storage storage) {
+        trainerStorage = storage.getTrainerStorage();
+        idCounter = trainerStorage
+                .keySet()
+                .stream()
+                .max(Long::compare)
+                .orElse(0L);
     }
 
     @Override
-    public Trainee update(Trainee trainee) {
-        return null;
+    public Trainer add(Trainer trainer) {
+        return trainerStorage.put(++idCounter, trainer);
     }
 
     @Override
-    public Trainee getById(long id) {
-        return null;
+    public Trainer update(Trainer trainer) {
+        return trainerStorage.put(idCounter, trainer);
+    }
+
+    @Override
+    public Optional<Trainer> getById(long id) {
+        return Optional.ofNullable(trainerStorage.get(id));
     }
 }
--- src/main/java/org/example/crmsystem/dao/TrainingRepositoryImpl.java
+++ src/main/java/org/example/crmsystem/dao/TrainingRepositoryImpl.java
@@ -1,15 +1,32 @@
 package org.example.crmsystem.dao;
 
-import org.example.crmsystem.model.Trainee;
+import org.example.crmsystem.Storage;
+import org.example.crmsystem.dao.interfaces.TrainingRepository;
+import org.example.crmsystem.model.Training;
+
+import java.util.HashMap;
+import java.util.Optional;
 
 public class TrainingRepositoryImpl implements TrainingRepository {
-    @Override
-    public Trainee add(Trainee trainee) {
-        return null;
+    private final HashMap<Long, Training> trainingStorage;
+    private long idCounter;
+
+    public TrainingRepositoryImpl(Storage storage) {
+        trainingStorage = storage.getTrainingStorage();
+        idCounter = trainingStorage
+                .keySet()
+                .stream()
+                .max(Long::compare)
+                .orElse(0L);
     }
 
     @Override
-    public Trainee getById(long id) {
-        return null;
+    public Training add(Training training) {
+        return trainingStorage.put(++idCounter, training);
+    }
+
+    @Override
+    public Optional<Training> getById(long id) {
+        return Optional.ofNullable(trainingStorage.get(id));
     }
 }
