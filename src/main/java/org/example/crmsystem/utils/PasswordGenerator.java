7d24692e4d7a733af27cd570bd9ad0a0e3d0d750
Kateryna Hratilova <gr.katrin.05@gmail.com>
2025-01-29 13:49:47 +0200

    config and utils

---
+++ src/main/java/org/example/crmsystem/utils/PasswordGenerator.java
@@ -0,0 +1,22 @@
+package org.example.crmsystem.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
+public class PasswordGenerator {
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int PASSWORD_LENGTH = 10;
+
+    public String generateUserPassword() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            password.append(ALPHABET.charAt(random.nextInt(ALPHABET.length())));
+        }
+
+        return password.toString();
+    }
+}
