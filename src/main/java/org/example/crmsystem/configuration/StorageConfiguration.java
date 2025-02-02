aa7c20b7e2c26d4e8bcf593f2cd28045e5ae6362
Kateryna Hratilova <gr.katrin.05@gmail.com>
2025-02-02 22:59:26 +0200

    config

--- src/main/java/org/example/crmsystem/configuration/StorageConfiguration.java
+++ src/main/java/org/example/crmsystem/configuration/StorageConfiguration.java
@@ -2,14 +2,16 @@ package org.example.crmsystem.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.example.crmsystem.model.Trainee;
+import org.example.crmsystem.model.Trainer;
+import org.example.crmsystem.model.Training;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import java.io.PrintStream;
-import java.util.Scanner;
+import java.util.HashMap;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.crmsystem")
@@ -29,5 +31,25 @@ public class StorageConfiguration {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         return mapper;
+    }
+
+    @Bean
+    public HashMap<Long, Trainee> traineeStorage(){
+        return new HashMap<Long, Trainee>();
+    }
+
+    @Bean
+    public HashMap<Long, Trainer> trainerStorage(){
+        return new HashMap<Long, Trainer>();
+    }
+
+    @Bean
+    public HashMap<Long, Training> trainingStorage(){
+        return new HashMap<Long, Training>();
+    }
+
+    @Bean
+    public String s(){
+        return "src/main/resources/trainee.json";
     }
 }
