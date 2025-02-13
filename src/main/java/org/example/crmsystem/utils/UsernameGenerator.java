315cffb8c40c03543b3f796a5b3b2503fe9bc2c1
Kateryna Hratilova <gr.katrin.05@gmail.com>
2025-02-13 16:41:42 +0200

    modified un generator

--- src/main/java/org/example/crmsystem/utils/UsernameGenerator.java
+++ src/main/java/org/example/crmsystem/utils/UsernameGenerator.java
@@ -2,7 +2,7 @@ package org.example.crmsystem.utils;
 
 import org.example.crmsystem.dao.interfaces.TraineeDAO;
 import org.example.crmsystem.dao.interfaces.TrainerDAO;
-import org.example.crmsystem.model.User;
+import org.example.crmsystem.entity.UserEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,9 +15,9 @@ public class UsernameGenerator {
         this.trainerDAO = trainerDAO;
     }
 
-    public String generateUserName(User user) {
-        String userName = user.getFirstName() + "." + user.getLastName();
-        int suffix = traineeDAO.getByUserName(userName).size() + trainerDAO.getByUserName(userName).size();
+    public String generateUserName(UserEntity userEntity) {
+        String userName = userEntity.getFirstName() + "." + userEntity.getLastName();
+        int suffix = traineeDAO.getWhereUserNameStartsWith(userName).size() + trainerDAO.getWhereUserNameStartsWith(userName).size();
         return suffix == 0 ? userName : userName + suffix;
     }
 }
