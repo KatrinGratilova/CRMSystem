bcd7b1f24fb022fd16ae0c73319b02add2d65597
Kateryna Hratilova <gr.katrin.05@gmail.com>
2025-02-13 16:42:20 +0200

    modified conf

--- src/main/java/org/example/crmsystem/configuration/StorageConfiguration.java
+++ src/main/java/org/example/crmsystem/configuration/StorageConfiguration.java
@@ -2,11 +2,11 @@ package org.example.crmsystem.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.logging.log4j.plugins.Singleton;
+import org.example.crmsystem.entity.*;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.crmsystem")
@@ -28,4 +28,26 @@ public class StorageConfiguration {
         return mapper;
     }
 
+    @Bean
+    @Singleton
+    public SessionFactory sessionFactory() {
+        org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+
+        configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/gym");
+        configuration.setProperty("hibernate.connection.username", "postgres");
+        configuration.setProperty("hibernate.connection.password", "230218");
+
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        configuration.setProperty("show_sql", true);
+        configuration.setProperty("hibernate.hbm2ddl.auto", "drop-and-create");
+
+        configuration.addAnnotatedClass(TrainingEntity.class);
+        configuration.addAnnotatedClass(TrainerEntity.class);
+        configuration.addAnnotatedClass(TraineeEntity.class);
+        configuration.addAnnotatedClass(TrainingTypeEntity.class);
+        configuration.addAnnotatedClass(UserEntity.class);
+
+        return configuration.buildSessionFactory();
+    }
 }
