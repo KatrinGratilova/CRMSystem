7f44d26b2074cc9960443e3040ba6d263938c5c5
Kateryna Hratilova <gr.katrin.05@gmail.com>
2025-02-28 23:14:33 +0200

    changed parameter in username generator

--- src/main/java/org/example/crmsystem/utils/UsernameGenerator.java
+++ src/main/java/org/example/crmsystem/utils/UsernameGenerator.java
@@ -2,7 +2,7 @@ package org.example.crmsystem.utils;
 
 import org.example.crmsystem.dao.interfaces.TraineeDAO;
 import org.example.crmsystem.dao.interfaces.TrainerDAO;
-import org.example.crmsystem.entity.UserEntity;
+import org.example.crmsystem.dto.user.UserServiceDTO;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,8 +15,8 @@ public class UsernameGenerator {
         this.trainerDAO = trainerDAO;
     }
 
-    public String generateUserName(UserEntity userEntity) {
-        String userName = userEntity.getFirstName() + "." + userEntity.getLastName();
+    public String generateUserName(UserServiceDTO userDTO) {
+        String userName = userDTO.getFirstName() + "." + userDTO.getLastName();
         int suffix = traineeDAO.getWhereUserNameStartsWith(userName).size() + trainerDAO.getWhereUserNameStartsWith(userName).size();
         return suffix == 0 ? userName : userName + suffix;
     }
