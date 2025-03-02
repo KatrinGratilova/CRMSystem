3284366eef107b2b93504d65f742a9d62679e463
Kateryna Hratilova <gr.katrin.05@gmail.com>
2025-03-02 21:27:11 +0200

    added global exception handler

---
+++ src/main/java/org/example/crmsystem/advicer/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package org.example.crmsystem.advicer;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.example.crmsystem.messages.ExceptionMessages;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleInvalidRequestException(MethodArgumentNotValidException ex) {
+        List<String> errorMessages = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        if (errorMessages.size() == 1) {
+            return new ResponseEntity<>(errorMessages.get(0), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleMissingRequestBody() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionMessages.INVALID_REQUEST_BODY.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleUserIsNotAuthenticated(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+//    @ExceptionHandler(org.springframework.web.server.ResponseStatusException.class)
+//    public ResponseEntity<String> handleNotFound(ResponseStatusException ex) {
+//        if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//                    .body("Page is not found.");
+//        }
+//        return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
+//    }
+}
